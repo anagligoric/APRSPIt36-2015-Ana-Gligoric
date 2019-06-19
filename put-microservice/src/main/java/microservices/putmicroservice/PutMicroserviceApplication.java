c47e7c1b21b56e80a8c69d209cfa29eead5312f0
anagligoric <anagligoric96@hotmail.com>
2019-06-19 10:46:58 +0200

    Put microservice

---
+++ put-microservice/src/main/java/microservices/putmicroservice/PutMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package microservices.putmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PutMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PutMicroserviceApplication.class, args);
+    }
+
+}
