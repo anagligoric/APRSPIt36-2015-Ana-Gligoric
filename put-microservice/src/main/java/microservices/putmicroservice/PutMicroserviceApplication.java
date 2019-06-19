3362001b039dcf5fb1f46b0678647e16fc630174
anagligoric <anagligoric96@hotmail.com>
2019-06-19 11:07:26 +0200

    Put and delete configuring

--- put-microservice/src/main/java/microservices/putmicroservice/PutMicroserviceApplication.java
+++ put-microservice/src/main/java/microservices/putmicroservice/PutMicroserviceApplication.java
@@ -2,7 +2,9 @@ package microservices.putmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PutMicroserviceApplication {
 
