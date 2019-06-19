a6c362582a88ceecb16c6da1255397b69be2cc67
anagligoric <anagligoric96@hotmail.com>
2019-06-19 02:33:02 +0200

    Eureka nameing microservice

--- get-microservice/src/main/java/microservices/getmicroservice/GetMicroserviceApplication.java
+++ get-microservice/src/main/java/microservices/getmicroservice/GetMicroserviceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GetMicroserviceApplication {
 
