1748c9f9ac172a22f34f41a7802deb9d8ef00bed
anagligoric <anagligoric96@hotmail.com>
2019-06-19 11:43:46 +0200

    Zuul API gateway

---
+++ zuul-api-gateway/src/main/java/microservices/zuulapigateway/ZuulLoggingFilter.java
@@ -0,0 +1,30 @@
+package microservices.zuulapigateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+
+public class ZuulLoggingFilter extends ZuulFilter {
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String filterType() {
+        // TODO Auto-generated method stub
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+}
